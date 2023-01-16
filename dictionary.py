5530dbff866889adf54b75cb07279f1f579356b9
Shivam-Kumar12 <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-01-16 22:06:13 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+mydict={
+         "Fast" :"IN A quick manner",
+         "Harry"  : "A Coder",
+         "shivam" : "is a good cooder",
+         "Marks" :  [1,2,5] ,
+         "another" :{'Shivam': 'player'} # nested dictionay
+      }
+str
+#print(mydict['Fast'])
+    
+#print(mydict['Harry'])
+mydict['Marks']= [45,67,7] # this defines that dictionary is a mutuable
+print(mydict['Marks'])
+#print(mydict['shivam'])
+
+#print(mydict['another']['Shiivam']) # nested dictionary
+ 
