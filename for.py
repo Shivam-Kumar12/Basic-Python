e26cf03d04cf80e8e84acbfdbe785eff249e519d
Shivam-Kumar12 <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-01 18:30:56 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,13 @@
+list=[23,34,45,76]
+for i in  list:
+    print(list)
+# range of a function
+print("range of a function") 
+for i in range(2,8): # start with 2
+     print(i)
+# for loop with else 
+print("for loop with else")
+for l in range(10):
+    print(l)
+else:
+    print("this the loop inside else of for")
