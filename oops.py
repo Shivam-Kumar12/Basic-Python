04ea238f264e1fed5b5d56ee6bd538f5b0522c4c
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-16 21:56:46 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,14 @@
+class Number:
+    def sum(self):
+     return self.a +self.b
+num=Number() # object instantination
+num.a=12
+num.b=34
+s=num.sum()
+print(s)
+
+
+
+
+
+#print("the sum of a and b",a+b)
